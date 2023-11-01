8bc506333551bcc884eab79e548e0fb69bf849ea
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-01 04:06:38 -0600

    Add popular job card routing, rendering

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import styles from './popularjobcard.style';
+import { checkImageUrl } from '../../../../utils/index';
+import { icons } from '../../../../constants';
 
-import styles from './popularjobcard.style'
-
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={ checkImageUrl(item.employer_logo)
+            ? {uri: item.employer_logo}
+            : icons.placeholder
+          }
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
-export default PopularJobCard
+export default PopularJobCard;
